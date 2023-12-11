d40d8bbb0817892a3ca82107243aadcfaff097fd
faisalzahoor1 <152835077+faisalzahoor1@users.noreply.github.com>
2023-12-12 01:08:21 +0500

    Update SolidRectangle.java

--- Patterns/SolidRectangle.java
+++ Patterns/SolidRectangle.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class SolidRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+              System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
