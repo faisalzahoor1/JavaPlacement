26c49e2b56e5c534c88381f51ead4bdaa5dc2e8e
faisalzahoor1 <152835077+faisalzahoor1@users.noreply.github.com>
2023-12-16 18:41:35 +0500

    Create ChartoIntandInttoChar.java

---
+++ StringsandStringBuilder/ChartoIntandInttoChar.java
@@ -0,0 +1,11 @@
+public class ChartoIntandInttoChar {
+    public static void main(String[] args) {
+        char str = 'h';
+        int s = (int)str;
+        System.out.println(s);
+
+        int b = 113;
+        Character d = (char)b;
+        System.out.println(d);
+    }
+}
