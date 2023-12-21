b16dbc8a25c03b37475d7a3a9c698cc3e5e6c3d7
faisalzahoor1 <152835077+faisalzahoor1@users.noreply.github.com>
2023-12-21 19:17:26 +0500

    Create NumPattern.java

---
+++ Patterns/NumPattern.java
@@ -0,0 +1,10 @@
+public class NumPattern {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
