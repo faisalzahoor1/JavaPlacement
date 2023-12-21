bb61f45d6fc9fe8ea2956b6f4e4d30c189f79dad
faisalzahoor1 <152835077+faisalzahoor1@users.noreply.github.com>
2023-12-21 19:15:47 +0500

    Create InverseHalfPyramid.java

---
+++ Patterns/InverseHalfPyramid.java
@@ -0,0 +1,10 @@
+public class InverseHalfPyramid {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
