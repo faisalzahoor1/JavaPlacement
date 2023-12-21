174907e85729a45e35c0ae1958376473bbb9d687
faisalzahoor1 <152835077+faisalzahoor1@users.noreply.github.com>
2023-12-21 19:16:49 +0500

    Create INvertNumPattern.java

---
+++ Patterns/INvertNumPattern.java
@@ -0,0 +1,10 @@
+public class INvertNumPattern {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <=4-i; j++) {
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
