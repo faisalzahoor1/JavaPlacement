9c435f66b5ed5ce2e1a265b67ba2f58d79797260
Muhammad Faisal Zahoor <152835077+faisalzahoor1@users.noreply.github.com>
2023-12-28 23:52:57 +0500

    Create IncreasingOrder.java

---
+++ Sorting/IncreasingOrder.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class qs4 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //int greatest = 0 ;
+        int min = 0;
+        int m = n-1;
+        int[] name = new int[n];
+        for (int i = 0; i < name.length; i++) {
+            name[i] = sc.nextInt();
+        }
+        
+        for (int i = 1; i <= name.length-1; i++) {
+            for (int j = 0; j < name.length; j++) {
+                if(name[i] <  name[j] ){
+                    int temp = name[i];
+                    name[i] = name[j];
+                    name[j] = temp;
+                }
+            }
+            
+        }
+        for (int j = 0; j < name.length; j++) {
+            System.out.println(name[j]);
+        }
+        
+    }
+} 
