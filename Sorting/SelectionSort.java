a3650552c8493fbcf110836189b83d3ded5ae085
Muhammad Faisal Zahoor <152835077+faisalzahoor1@users.noreply.github.com>
2024-01-30 07:53:25 +0500

    Create SelectionSort.java

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,20 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = { 7, 8, 3, 1, 2 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[smallest];
+            arr[smallest] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
